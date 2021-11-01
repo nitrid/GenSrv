bbaea0d2d6797df3fcc49a615868b06163befa80
Mahir Gündüz <mhrgndz171@gmail.com>
2021-11-01 17:20:05 +0300

    ..

--- bin/www/core/react/devex/grid.js
+++ bin/www/core/react/devex/grid.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import DataGrid from 'devextreme-react/data-grid';
-import { datatable } from '../../core.js';
-import CustomStore from 'devextreme/data/custom_store';
 import Base from './base.js';
 
 export default class NdGrid extends Base
@@ -14,7 +12,6 @@ export default class NdGrid extends Base
         this.state = 
         {            
             showBorders : typeof props.showBorders == 'undefined' ? false : props.showBorders,
-            data : typeof props.data == 'undefined' ? undefined : props.data,
             columnsAutoWidth : typeof props.columnsAutoWidth == 'undefined' ? false : props.columnsAutoWidth,
             allowColumnReordering : typeof props.allowColumnReordering == 'undefined' ? false : props.allowColumnReordering,
             allowColumnResizing : typeof props.allowColumnResizing == 'undefined' ? false : props.allowColumnResizing,
@@ -43,6 +40,8 @@ export default class NdGrid extends Base
         this._onSaved = this._onSaved.bind(this);
         this._onEditCanceling = this._onEditCanceling.bind(this);
         this._onEditCanceled = this._onEditCanceled.bind(this);
+
+        console.log(this.props)
     }
     //#region Private
     _onInitialized(e) 
@@ -156,101 +155,6 @@ export default class NdGrid extends Base
         {
             await this.dataRefresh(this.state.data)                 
         }
-    }
-    dataRefresh(e)
-    {        
-        return new Promise(mresolve => 
-        {
-            let tmpThis = this;
-            this.setState(
-            { 
-                data : 
-                {
-                    store : new CustomStore(
-                    {
-                        load: () =>
-                        {                        
-                            return new Promise(async resolve => 
-                            {        
-                                // EĞER FONKSİYONA PARAMETRE GÖNDERİLMEMİŞ İSE VE STATE DEĞİŞKENİNDE DAHA ÖNCEDEN ATANMIŞ DATA SOURCE VARSA GRİD REFRESH EDİLİYOR.
-                                if(typeof e == 'undefined' && typeof tmpThis.state.data != 'undefined' && typeof tmpThis.state.data.source != 'undefined')
-                                {
-                                    e = 
-                                    {
-                                        source : tmpThis.state.data.source
-                                    }
-                                }
-                                // EĞER DATA SOURCE A DİZİ GÖNDERİLMİŞ İSE
-                                if(typeof e.source != 'undefined' && Array.isArray(e.source))
-                                {
-                                    tmpThis.state.data.source = e.source;
-                                    tmpThis.state.data.datatable = new datatable();
-                                    tmpThis.state.data.datatable.import(e.source)
-                                    resolve({data: tmpThis.state.data.datatable.toArray()});
-                                    mresolve()
-                                }
-                                // EĞER DATA SOURCE A DATATABLE GÖNDERİLMİŞ İSE
-                                else if (typeof e.source != 'undefined' && e.source instanceof datatable)
-                                {
-                                    tmpThis.state.data.source = e.source;
-                                    tmpThis.state.data.datatable = e.source;
-                                    await tmpThis.state.data.datatable.refresh();
-                                    resolve({data: tmpThis.state.data.datatable.toArray()});
-                                    mresolve()
-                                }
-                                // EĞER DATA SOURCE A QUERY SET GÖNDERİLMİŞ İSE
-                                else if (typeof e.source != 'undefined' && typeof e.source == 'object' && typeof e.source.sql != 'undefined' && typeof e.source.select != 'undefined')
-                                {                
-                                    tmpThis.state.data.source = e.source;
-                                    tmpThis.state.data.datatable = new datatable();
-                                    tmpThis.state.data.datatable.sql = e.source.sql
-                                    tmpThis.state.data.datatable.selectCmd = e.source.select;
-                                    tmpThis.state.data.datatable.insertCmd = e.source.insert;
-                                    tmpThis.state.data.datatable.updateCmd = e.source.update;
-                                    tmpThis.state.data.datatable.deleteCmd = e.source.delete;
-
-                                    await tmpThis.state.data.datatable.refresh()
-                                    resolve({data: tmpThis.state.data.datatable.toArray()});
-                                    mresolve()
-                                }
-                                else
-                                {
-                                    resolve({data: []});
-                                    mresolve()
-                                }
-                            });
-                        },
-                        insert: (values) => 
-                        {
-                            return new Promise(async resolve => 
-                            {
-                                if(typeof tmpThis.state.data != 'undefined' && typeof tmpThis.state.data.datatable != 'undefined')
-                                {
-                                    tmpThis.state.data.datatable.push(values)
-                                    await tmpThis.state.data.datatable.insert();
-                                }
-                                resolve()                                
-                            });
-                        },
-                        update: (key, values) => 
-                        {
-                            return new Promise(async resolve => 
-                            {
-                                if(typeof tmpThis.state.data != 'undefined' && typeof tmpThis.state.data.datatable != 'undefined')
-                                {
-                                    for (let i = 0; i < Object.keys(values).length; i++) 
-                                    {
-                                        tmpThis.state.data.datatable.find(x => x === key)[Object.keys(values)[i]] = values[Object.keys(values)[i]]
-                                    }                                    
-                                }
-                                await this.state.data.datatable.update();
-                                resolve()                                
-                            });
-                        },
-                    })
-                }
-            });
-        });
     }
     getSelectedData()
     {
