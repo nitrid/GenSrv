d84a64f93d093e817051d7cd8590592703a101c9
nitrid <alikemalkaraca@gmail.com>
2022-12-21 16:50:22 +0100

    1.0.94

--- test/test.js
+++ test/test.js
@@ -15,64 +15,84 @@ async function run()
     let tmpcore = new core(io('http://localhost',{timeout:100000}));
     await tmpcore.auth.login('ATest','1','POS')
     
-
-    let tmpDt = new datatable()
-    tmpDt.insertCmd = 
-    {
-        query : "EXEC [dbo].[PRD_POS_SALE_INSERT_TEST] @CUSER = 'ALOOO', @AMOUNT = @PAMOUNT, @FAMOUNT = @PFAMOUNT, @VAT = @PVAT, @TOTAL = @PTOTAL, @DISCOUNT = @PDISCOUNT, @LOYALTY = @PLOYALTY,@POS = @PPOS,@PROMO_TYPE = @PPROMO_TYPE", 
-        param : ['PAMOUNT:float','PFAMOUNT:float','PVAT:float','PTOTAL:float','PDISCOUNT:float','PLOYALTY:float','PPOS:string|50','PPROMO_TYPE:int'],
-        dataprm : ['AMOUNT','FAMOUNT','VAT','TOTAL','DISCOUNT','LOYALTY','POS','PROMO_TYPE']
-    } 
-
-    let myFn = (p) =>
-    {
-        return setTimeout(() => 
+    let x = await tmpcore.sql.execute
+    (
         {
-            console.log(p)
-        }, 2000);
-    }
+            query:"SELECT * FROM ITEMS",
+            connection:
+            {
+                server: "192.168.1.75",
+                database: "NANCY",
+                user: "piqpos", 
+                password: "1122334455", 
+                connectionTimeout: 15000,
+                requestTimeout: 15000,
+                options: 
+                {
+                    trustedConnection: false,
+                    trustServerCertificate: true
+                }              
+            }
+        }
+    )
+    console.log(x)
+    // let tmpDt = new datatable()
+    // tmpDt.insertCmd = 
+    // {
+    //     query : "EXEC [dbo].[PRD_POS_SALE_INSERT_TEST] @CUSER = 'ALOOO', @AMOUNT = @PAMOUNT, @FAMOUNT = @PFAMOUNT, @VAT = @PVAT, @TOTAL = @PTOTAL, @DISCOUNT = @PDISCOUNT, @LOYALTY = @PLOYALTY,@POS = @PPOS,@PROMO_TYPE = @PPROMO_TYPE", 
+    //     param : ['PAMOUNT:float','PFAMOUNT:float','PVAT:float','PTOTAL:float','PDISCOUNT:float','PLOYALTY:float','PPOS:string|50','PPROMO_TYPE:int'],
+    //     dataprm : ['AMOUNT','FAMOUNT','VAT','TOTAL','DISCOUNT','LOYALTY','POS','PROMO_TYPE']
+    // } 
+
+    // let myFn = (p) =>
+    // {
+    //     return setTimeout(() => 
+    //     {
+    //         console.log(p)
+    //     }, 2000);
+    // }
     
 
-    setTimeout(async() => 
-    {
-        let m0 = myFn(100)
+    // setTimeout(async() => 
+    // {
+    //     let m0 = myFn(100)
 
-        tmpDt.push({AMOUNT:1,FAMOUNT:1,VAT:0,TOTAL:2,DISCOUNT:0,LOYALTY:0,POS:'00000000-0000-0000-0000-000000000000',PROMO_TYPE:0})
-        let x = await tmpDt.update()
+    //     tmpDt.push({AMOUNT:1,FAMOUNT:1,VAT:0,TOTAL:2,DISCOUNT:0,LOYALTY:0,POS:'00000000-0000-0000-0000-000000000000',PROMO_TYPE:0})
+    //     let x = await tmpDt.update()
 
-        if (x == 0)
-        {
-            console.log(10)
-            clearTimeout(m0)
-        }
+    //     if (x == 0)
+    //     {
+    //         console.log(10)
+    //         clearTimeout(m0)
+    //     }
         
-    }, 1000);
+    // }, 1000);
     
-    setTimeout(async() => 
-    {
-        let m1 = myFn(101)
+    // setTimeout(async() => 
+    // {
+    //     let m1 = myFn(101)
 
-        tmpDt.push({AMOUNT:1,FAMOUNT:1,VAT:0,TOTAL:2,DISCOUNT:0,LOYALTY:0,POS:'00000000-0000-0000-0000-000000000000',PROMO_TYPE:1})
-        let x = await tmpDt.update()
-        if (x == 0)
-        {
-            console.log(11)
-            clearTimeout(m1)
-        }
-    }, 1000);
+    //     tmpDt.push({AMOUNT:1,FAMOUNT:1,VAT:0,TOTAL:2,DISCOUNT:0,LOYALTY:0,POS:'00000000-0000-0000-0000-000000000000',PROMO_TYPE:1})
+    //     let x = await tmpDt.update()
+    //     if (x == 0)
+    //     {
+    //         console.log(11)
+    //         clearTimeout(m1)
+    //     }
+    // }, 1000);
 
-    setTimeout(async() => 
-    {
-        let m2 = myFn(102)
+    // setTimeout(async() => 
+    // {
+    //     let m2 = myFn(102)
 
-        tmpDt.push({AMOUNT:1,FAMOUNT:1,VAT:0,TOTAL:2,DISCOUNT:0,LOYALTY:0,POS:'00000000-0000-0000-0000-000000000000',PROMO_TYPE:0})
-        let x = await tmpDt.update()
-        if (x == 0)
-        {
-            console.log(12)
-            clearTimeout(m2)
-        }
-    }, 1000);
+    //     tmpDt.push({AMOUNT:1,FAMOUNT:1,VAT:0,TOTAL:2,DISCOUNT:0,LOYALTY:0,POS:'00000000-0000-0000-0000-000000000000',PROMO_TYPE:0})
+    //     let x = await tmpDt.update()
+    //     if (x == 0)
+    //     {
+    //         console.log(12)
+    //         clearTimeout(m2)
+    //     }
+    // }, 1000);
     
 }
 
